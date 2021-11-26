f986e865352b1b0eb390a5444d63f21e1cd95c8d
MichelleTheDragon <mh@maoeh.dk>
2021-11-26 13:13:54 +0100

    solution add

---
+++ Auroria/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auroria
+{
+    public abstract class GameObject
+    {
+        protected Texture2D sprite;
+
+        protected float scale = 1.0f;
+        protected float layer = 1.0f;
+        protected Vector2 worldPos;
+
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
