3554dfd9437aeb96ccf89f185cc22fd81f944987
MichelleTheDragon <mh@maoeh.dk>
2021-11-29 11:32:52 +0100

    GameObject add

--- Auroria/Input.cs
+++ Auroria/Input.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,36 @@ namespace Auroria
     class Input
     {
 
-        public void Update(GameTime gameTime)
+        public void Update(GameTime gameTime, PlayerObject player)
         {
 
+            Movement(gameTime, player);
+        }
+
+        private void Movement(GameTime gameTime, PlayerObject player)
+        {
+            KeyboardState keyState = Keyboard.GetState(); //get state of keyboard
+
+            //move selection on grid, W = up, S = down, A = left, D = right
+            if (!(keyState.IsKeyDown(Keys.W) && keyState.IsKeyDown(Keys.S)))
+            {
+                if (keyState.IsKeyDown(Keys.W))
+                {
+
+                }
+                if (keyState.IsKeyDown(Keys.S))
+                {
+
+                }
+            }
+            else if (keyState.IsKeyDown(Keys.A))
+            {
+
+            }
+            else if (keyState.IsKeyDown(Keys.D))
+            {
+
+            }
         }
     }
 }
--- Auroria/PlayerObject.cs
+++ Auroria/PlayerObject.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Auroria
 {
-    class PlayerObject
+    class PlayerObject : GameObject
     {
+        private float speed = 10.0f;
+        public float Speed { get { return speed; } }
+        private Vector2 movement = Vector2.Zero;
+        public Vector2 Movement { get { return movement; } set { movement = value } }
+
+        public PlayerObject(Texture2D sprite, Vector2 worldPos) : base(sprite, worldPos)
+        {
+
+        }
     }
 }
