445497382d262402b9f72eeb77884b52b1857d57
MichelleTheDragon <mh@maoeh.dk>
2021-11-30 13:59:02 +0100

    Update WorldAssembler.cs

--- Auroria/WorldAssembler.cs
+++ Auroria/WorldAssembler.cs
@@ -31,28 +31,28 @@ namespace Auroria
             map.GetData(colors); //sets those colors in the array
 
             Color black = new Color(0, 0, 0); //creates a black color
-            Color green = new Color(0, 164, 19); //creates a black color
-            Color yellowGreen = new Color(186, 175, 1); //creates a black color
-            Color blue = new Color(72, 0, 255); //creates a black color
+            Color green = new Color(0, 164, 19);
+            Color yellowGreen = new Color(186, 175, 1); 
+            Color blue = new Color(72, 0, 255);
 
             for (int y = 0; y < mapHeight; y++)
             {
                 for (int x = 0; x < mapWidth; x++)
                 {
                     gameWorld.AddObject(new Rock(tileSet, grass, new Vector2(x * 64, y * 64)));
-                    if (colors[y * mapWidth + x] == black) //if the checked pixel is black
+                    if (colors[y * mapWidth + x] == black) 
                     {
                         gameWorld.AddObject(new Rock(tileSet, rock, new Vector2(x * 64, y * 64)));
                     }
-                    else if (colors[y * mapWidth + x] == green) //if the checked pixel is black
+                    else if (colors[y * mapWidth + x] == green) 
                     {
                         gameWorld.AddObject(new Rock(tileSet, tree, new Vector2(x * 64, y * 64)));
                     }
-                    else if (colors[y * mapWidth + x] == yellowGreen) //if the checked pixel is black
+                    else if (colors[y * mapWidth + x] == yellowGreen)
                     {
                         gameWorld.AddObject(new Rock(tileSet, plant, new Vector2(x * 64, y * 64)));
                     }
-                    else if (colors[y * mapWidth + x] == blue) //if the checked pixel is black
+                    else if (colors[y * mapWidth + x] == blue) 
                     {
                         gameWorld.AddObject(new Rock(tileSet, flora, new Vector2(x * 64, y * 64)));
                     }
