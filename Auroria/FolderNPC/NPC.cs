d6dc330c6668fa23a5fe4fba5f63551d691117dd
mettedknudsen <57904711+mettedknudsen@users.noreply.github.com>
2021-12-15 23:18:18 +0100

    npc

--- Auroria/FolderNPC/NPC.cs
+++ Auroria/FolderNPC/NPC.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Auroria
 {
-    class Class1
+    class NPC : GameObject
     {
+        #region Fields
+
+        private string name;
+        public string Name { get { return name; } }
+
+        private string dialog;
+        public string Dialog { get { return dialog; } }
+        #endregion
+
+        #region Constructor
+        public NPC(Texture2D sprite, int[] tilePos, Vector2 worldPos, bool isSolid) : base(sprite, tilePos, worldPos, isSolid)
+        {
+        }
+        #endregion
+
+        #region Methods
+        public override void Update(GameTime gameTime)
+        {
+        }
+        #endregion
+
     }
 }
