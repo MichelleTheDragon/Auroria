2b04ce729046351e216f3cd933c24379c37ca127
mettedknudsen <57904711+mettedknudsen@users.noreply.github.com>
2021-12-20 13:44:09 +0100

    more confusion

---
+++ Auroria/Source/Engine/Menu2d.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auroria.Source.Engine
+{
+    public class Menu2d
+    {
+        protected bool active;
+
+        public Vector2 pos, dims, topLeft;
+
+        public Animated2d bkg;
+
+        public Button2d closeBtn;
+
+        public SpriteFont font;
+
+        public PassObject CloseAction;
+
+        public Menu2d(Vector2 POS, Vector2 DIMS, PassObject CLOSEACTION)
+        {
+            pos = POS;
+            dims = DIMS;
+            CloseAction = CLOSEACTION;
+
+            bkg = new Animated2d("", new Vector2(), new Vector2(dims.X, dims.Y), new Vector2(1, 1), Color.White)
+                ;
+        }
+    }
+}
